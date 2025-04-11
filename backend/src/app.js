fe4b8bb5299035148a0b872160ca288e57124941
kuroi-kurlik <kuroi-kurlik@proton.me>
2025-04-12 01:58:25 +0900

    deleting console.log

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,6 @@ const { cors } = require("./config");
 const path = require("path");
 const app = express();
 
-console.log("CORS middleware: ", cors);
 app.use(cors)
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
